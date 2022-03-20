ed9f94fa221df48a10e26d3c102af7d90ae145d2
tnnrhpwd <tannerh@engineer.com>
2022-03-20 12:59:58 -0500

    Initial upload

---
+++ CompoundInterestCalcBrains.js
@@ -0,0 +1,101 @@
+let title= document.getElementById("title");
+let description= document.getElementById("description");
+let inputs= document.getElementById("inputs");
+let button= document.getElementById("button");
+let output= document.getElementById("output");
+var inputString;
+var outputString="";
+var tense = "";
+
+Initialize()
+
+
+function Initialize(){
+    title.innerHTML += `<div id="title">`+"Online Financial Annuity Calculator"
+    +`</div>`; // description
+
+    description.innerHTML +=`<div id="description">`+"Take advantage of this calculator to simulate compound interest over time and convert annuities between forms!"
+    +`<br><br>`+ "P=Present, F=Future, A=Annual, and G=Gradient"+`<br><br>`
+    +"Simply enter the type of calculation you want: PtoF, AtoF, FtoP, AtoP, GtoP, FtoA, PtoA, or GtoA.<br> Next, enter the <b>$amount</b>. <br>Then, enter the desired <b>number of periods</b>. <br>Lastly, enter the <b>interest rate</b>. <br><br> Example: If you wanted to calculate the future value at 15% annualized interest over 10yrs. from a present value of $10,000, then the input would be: <br><br> ptof 10000 10 .15"
+    +`</div>`; // description
+
+    inputs.innerHTML +=`<input type="text" id="inputTEXT"/>`;
+    inputs.innerHTML +=`<button id="button" onclick="Calculate()" >Run</button>`;
+}
+
+
+
+function Calculate(){//This function outputs the answer based on input
+    // create a variable of the input field
+    inputString =document.getElementById("inputTEXT").value;
+    // parse data string apart
+    var words=inputString.split(' ');
+    let fromFrom = words[0];
+    let fromAmount = words[1];
+    let fromPeriods = words[2];
+    let fromRate = words[3];
+    // calculates the value
+    outputString= Calculations(fromFrom,fromAmount,fromPeriods,fromRate);
+    //outputs the answer
+    output.innerHTML= `<div id="output">`+"The "+tense+" value is $"+outputString+"."
+    +`</div>`; // description
+};
+
+
+function Calculations(toFrom, toAmount, toPeriods, toRate){
+   var type = toFrom.toUpperCase();
+   var amount = toAmount;
+   var n = toPeriods;
+   var i = toRate;
+   var ansr = 0;
+   tense = "";
+   //System.out.println(\"Type= \" + type + \" , Amount= \" + amount + \", Periods= \" + n + \", Interest= \" + i);
+    if(type==("PTOF")) // This method finds the future worth from the present worth
+    {
+       tense = "future";
+       ansr = (amount * (Math.pow(1+i,n))).toFixed(2);
+    }
+    else if(type==("ATOF")) // This method finds the future worth from the annual worth
+    {
+        tense = "future";
+        ansr = (amount * (((Math.pow((1+i),n))-1)/i)).toFixed(2);
+    }
+    else if(type==("FTOP")) // This method finds the present worth from the future worth
+    {
+        tense = "present";
+        ansr = (amount * (Math.pow(1/(1+i),n))).toFixed(2);
+    }
+    else if(type==("ATOP")) // This method finds the present worth from the annual worth
+    {
+        tense = "present";
+        ansr = (amount * (((Math.pow((1+i),n))-1)/(i*(Math.pow((1+i),n))))).toFixed(2);
+    }
+    else if(type==("GTOP")) // This method finds the present worth from the gradient worth
+    {
+        tense = "present";
+        var innerL = ((Math.pow((1+i),n)-1)/(i*(Math.pow((1+i),n))));
+        var innerR = n/(Math.pow((1+i),n));
+        ansr = (amount * (1/i)*(innerL - innerR)).toFixed(2);
+    }
+    else if(type==("FTOA")) // This method finds the annual worth from the future worth
+    {
+        tense = "annual";
+        ansr = (amount * ((i)/(Math.pow((1+i),n)-1))).toFixed(2);
+    }
+    else if(type==("PTOA")) // This method finds the annual worth from the present worth
+    {
+        tense = "annual";
+        var upper = i*(Math.pow((1+i),n));
+        var lower = (Math.pow((1+i),n)-1);
+        ansr = (upper/lower).toFixed(2);
+    }
+    else if(type==("GTOA")) // This method finds the annual worth from the gradient worth
+    {
+        tense = "annual";
+        ansr = (amount * ((1/i)-(n/(Math.pow((1+i),n)-1)))).toFixed(2);
+    }
+    else {ansr="Please reread the directions."; }
+    return ansr
+}
+
+
