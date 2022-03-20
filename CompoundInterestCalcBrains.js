6dc179a810769f6bf72acd1fbdf3d36f63e15adc
tnnrhpwd <tannerh@engineer.com>
2022-03-20 13:20:40 -0500

    Added many comments and organized the code.

--- CompoundInterestCalcBrains.js
+++ CompoundInterestCalcBrains.js
@@ -1,31 +1,30 @@
+// assign variables for HTML outputs
 let title= document.getElementById("title");
 let description= document.getElementById("description");
 let inputs= document.getElementById("inputs");
 let button= document.getElementById("button");
 let output= document.getElementById("output");
+// Initalize strings used for input collection and output
 var inputString;
 var outputString="";
 var tense = "";
 
-Initialize()
+Initialize() // call the main method that contains the webpage text
 
-
-function Initialize(){
+function Initialize(){ // the main method that contains the webpage text
     title.innerHTML += `<div id="title">`+"Online Financial Annuity Calculator"
-    +`</div>`; // description
+    +`</div>`; // add the title text
 
     description.innerHTML +=`<div id="description">`+"Take advantage of this calculator to simulate compound interest over time and convert annuities between forms!"
     +`<br><br>`+ "P=Present, F=Future, A=Annual, and G=Gradient"+`<br><br>`
     +"Simply enter the type of calculation you want: PtoF, AtoF, FtoP, AtoP, GtoP, FtoA, PtoA, or GtoA.<br> Next, enter the <b>$amount</b>. <br>Then, enter the desired <b>number of periods</b>. <br>Lastly, enter the <b>interest rate</b>. <br><br> Example: If you wanted to calculate the future value at 15% annualized interest over 10yrs. from a present value of $10,000, then the input would be: <br><br> ptof 10000 10 .15"
-    +`</div>`; // description
+    +`</div>`; // add the description text
 
-    inputs.innerHTML +=`<input type="text" id="inputTEXT"/>`;
-    inputs.innerHTML +=`<button id="button" onclick="Calculate()" >Run</button>`;
+    inputs.innerHTML +=`<input type="text" id="inputTEXT"/>`; // add the textbox
+    inputs.innerHTML +=`<button id="button" onclick="Calculate()" >Run</button>`; // add the run button
 }
 
-
-
-function Calculate(){//This function outputs the answer based on input
+function Calculate(){          // EVERY BUTTON PRESS - outputs the answer based on the data in the textbox
     // create a variable of the input field
     inputString =document.getElementById("inputTEXT").value;
     // parse data string apart
@@ -34,22 +33,20 @@ function Calculate(){//This function outputs the answer based on input
     let fromAmount = words[1];
     let fromPeriods = words[2];
     let fromRate = words[3];
-    // calculates the value
-    outputString= Calculations(fromFrom,fromAmount,fromPeriods,fromRate);
-    //outputs the answer
+    outputString= Calculations(fromFrom,fromAmount,fromPeriods,fromRate);  // calculates the value
     output.innerHTML= `<div id="output">`+"The "+tense+" value is $"+outputString+"."
-    +`</div>`; // description
+    +`</div>`;    //outputs the answer
 };
 
+function Calculations(toFrom, toAmount, toPeriods, toRate){ // called from Calculate() - does math based on inputs
+   var type = toFrom.toUpperCase(); // what formula to use
+   var amount = toAmount;           // amount of resources or currency
+   var n = toPeriods;               // number of periods that interest is applied
+   var i = toRate;                  // interest rate or discount rate
+   var ansr = 0;                    // output
+   tense = "";                      // what time tense to use -- future, present, or past
 
-function Calculations(toFrom, toAmount, toPeriods, toRate){
-   var type = toFrom.toUpperCase();
-   var amount = toAmount;
-   var n = toPeriods;
-   var i = toRate;
-   var ansr = 0;
-   tense = "";
-   //System.out.println(\"Type= \" + type + \" , Amount= \" + amount + \", Periods= \" + n + \", Interest= \" + i);
+   // all of these formulas were manually entered from my Engineering economy textbook - (Engineering Economy - Sullivan, Wicks, Koelling (2015))
     if(type==("PTOF")) // This method finds the future worth from the present worth
     {
        tense = "future";
@@ -94,8 +91,8 @@ function Calculations(toFrom, toAmount, toPeriods, toRate){
         tense = "annual";
         ansr = (amount * ((1/i)-(n/(Math.pow((1+i),n)-1)))).toFixed(2);
     }
-    else {ansr="Please reread the directions."; }
-    return ansr
+    else {ansr="Please reread the directions."; } // if bad input
+    return ansr // return the answer
 }
 
 
